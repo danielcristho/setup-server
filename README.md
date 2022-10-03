12683ed10869a61063a78cc71d8fda74a979a0bd
danielcristho <pepuhodaniel93.learn@gmail.com>
2022-10-03 23:06:17 +0700

    feat(shell-script): Add new command

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # setup-server
 Automation Tool for Server Setup
+
+Feel free to modify my codes
--- shell-script/install.sh
+++ shell-script/install.sh
@@ -14,48 +14,49 @@ echo " 5. Install PHP8.0                                              ";
 echo " 6. Install DNS-Server                                          ";
 echo " 0. Exit                                                        ";
 echo "================================================================";
-read -p " Masukan Nomor Pilihan Anda [0 - 6] : " choice;
+
+read -p " Enter Your Choice Number [0 - 6] : " choice;
 echo "";
 case $choice in
-1)  read -p "Anda akan mengupdate machine ini? y/n :" -n 1 -r
+
+1)  read -p "You will update this machine? y/n :" -n 1 -r
     echo ""
     if [[ ! $REPLY =~ ^[Nn]$ ]]
     then 
     sudo apt update
     sudo apt-get install net-tools -y
-    echo "Update berhasil"
+    echo "Update success"
     fi
     ;;
 
-2)  read -p "Apakah Anda akan menginstall Nginx? y/n :" -n 1 -r
+2)  read -p "You want install Nginx? y/n :" -n 1 -r
     echo ""
     if [[ ! $REPLY =~ ^[Nn]$ ]]
     then 
     sudo apt-get install nginx -y
-    echo "Nginx berhasil terinstall"
+    echo "Nginx is ready to use"
     fi
     ;; 
 
-3)  read -p "Apakah Anda akan menginstall Nginx? y/n :" -n 1 -r
+3)  read -p "You want install Apache? y/n :" -n 1 -r
     echo ""
     if [[ ! $REPLY =~ ^[Nn]$ ]]
     then 
     sudo apt-get install apache2 -y
-    echo "Apache berhasil terinstall"
+    echo "Apache is ready to use"
     fi
     ;; 
 
-
-4)  read -p "Apakah Anda akan menginstall MySQL? y/n :" -n 1 -r
+4)  read -p "You want install MySQL? y/n :" -n 1 -r
     echo ""
     if [[ ! $REPLY =~ ^[Nn]$ ]]
     then 
     sudo apt-get install mysql-server -y
-    echo "MySQL berhasil terinstall"
+    echo "MySQL is ready to use"
     fi
     ;;
 
-5)  read -p "Apakah Anda akan menginstall PHP? y/n :" -n 1 -r
+5)  read -p "You want install PHP? y/n :" -n 1 -r
     echo ""
     if [[ ! $REPLY =~ ^[Nn]$ ]]
     then 
@@ -63,11 +64,11 @@ case $choice in
     sudo apt update
     echo "Add PHP Repository"
     sudo apt-get install php8.0-common php8.0-cli php8.0-mbstring php8.0-xml php8.0-curl php8.0-mysql php8.0-fpm -y
-    echo "PHP berhasil terinstall"
+    echo "PHP is ready to use"
     fi
     ;;
 
-6)  read -p "Apakah Anda akan menginstall bind9? y/n :" -n 1 -r
+6)  read -p "You want install bind9? y/n :" -n 1 -r
     echo ""
     if [[ ! $REPLY =~ ^[Nn]$ ]]
     then 
@@ -77,14 +78,14 @@ case $choice in
 
 0) exit
     ;;
-*)    echo "Maaf, menu tidak ada"
+*)    echo "sorry, menu is not found"
 esac
-echo -n "Kembali ke menu? [y/n]: ";
+echo -n "Back again? [y/n]: ";
 read again;
 while [[ $again != 'Y' ]] && [[ $again != 'y' ]] && [[ $again != 'N' ]] && [[ $again != 'n' ]];
 do
-echo "Masukkan yang anda pilih tidak ada di menu";
-echo -n "Kembali ke menu? [y/n]: ";
+echo "Your input is not correct";
+echo -n "back again? [y/n]: ";
 read again;
 done
 done
